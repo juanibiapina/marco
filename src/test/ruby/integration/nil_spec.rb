35ba5afe2f38ecdee5376675c6b579e1eacf6959
Juan Ibiapina <juanibiapina@gmail.com>
2013-08-13 00:09:51 -0300

    Remove silly tests.

--- src/test/ruby/integration/nil_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'helpers/integration'
-
-describe "nil" do
-  it "returns itself" do
-    run "nil"
-
-    expect(result).to eq("nil")
-  end
-end
--- src/test/ruby/integration/string_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'helpers/integration'
-
-describe "a string" do
-  it "returns itself" do
-    run %q("sample string")
-
-    expect(result).to eq(%q("sample string"))
-  end
-end
