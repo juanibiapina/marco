6ec2eb3a4a88d3905d8306a36307dc43a0edcd8f
Juan Ibiapina <juanibiapina@gmail.com>
2013-08-18 16:41:39 -0300

    Move message test.

--- src/test/java/marco/lang/MessageTest.java
+++ src/test/java/marco/lang/MessageTest.java
@@ -7,11 +7,11 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-public class MarcoInterpreterTest {
+public class MessageTest {
     private MarcoRuntime runtime = new MarcoRuntime();
 
     @Test
-    public void testMessage() throws Exception {
+    public void nonExistentMessage() throws Exception {
         try {
             runtime.interpreter.eval(runtime.global, code("simplemessage"));
         } catch (MarcoException e) {
