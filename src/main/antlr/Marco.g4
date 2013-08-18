49a0562cac1e6b475f037cf76a2f30349c30fa75
Juan Ibiapina <juanibiapina@gmail.com>
2013-08-18 18:04:08 -0300

    Add support for dashes in messages.

--- src/main/antlr/Marco.g4
+++ src/main/antlr/Marco.g4
@@ -2,4 +2,4 @@ grammar Marco;
 
 program : ID ;
 
-ID : [a-z_]+ ;
+ID : [a-z_-]+ ;
--- src/test/groovy/Message.groovy
+++ src/test/groovy/Message.groovy
@@ -18,4 +18,13 @@ class Message extends IntegrationSpecification {
         MarcoException e = thrown()
         assert e.getMessage() == "Exception: Global does not respond to message_with_underscores"
     }
+
+    def "messages can have dashes"() {
+        when:
+        eval("message-with-dashes")
+
+        then:
+        MarcoException e = thrown()
+        assert e.getMessage() == "Exception: Global does not respond to message-with-dashes"
+    }
 }  
