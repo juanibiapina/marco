59fc04f7208e3836a5999a507c71a34fd5b4dc50
Juan Ibiapina <juanibiapina@gmail.com>
2013-09-09 23:42:38 -0300

    Refactor interpreter.

--- src/main/java/marco/lang/MarcoInterpreter.java
+++ src/main/java/marco/lang/MarcoInterpreter.java
@@ -5,21 +5,21 @@ import marco.lang.values.Cast;
 import java.util.List;
 
 public class MarcoInterpreter {
-    public MarcoObject evalLine(MarcoObject context, MarcoObject chain) {
+    public MarcoObject eval(MarcoObject context, MarcoObject chainList) {
+        MarcoObject lastResult = context;
+        List<MarcoObject> chainsList = Cast.toList(chainList);
+        for (MarcoObject chain : chainsList) {
+            lastResult = evalChain(context, chain);
+        }
+        return lastResult;
+    }
+
+    private MarcoObject evalChain(MarcoObject context, MarcoObject chain) {
         MarcoObject on = context;
         List<MarcoObject> messages = Cast.toList(chain.sendMessage(context, "messages"));
         for (MarcoObject message : messages) {
             on = on.sendMessage(context, message);
         }
         return on;
-    }
-
-    public MarcoObject eval(MarcoObject context, MarcoObject chainList) {
-        MarcoObject lastResult = context;
-        List<MarcoObject> chainsList = Cast.toList(chainList);
-        for (MarcoObject chain : chainsList) {
-            lastResult = evalLine(context, chain);
-        }
-        return lastResult;
     }
 }
