5310e7d8f76eb7e1a14ed05827d6c0d75144fee6
Juan Ibiapina <juanibiapina@gmail.com>
2013-11-28 22:22:01 -0200

    Minor fixes.

--- src/test/groovy/Lists.groovy
+++ src/test/groovy/Lists.groovy
@@ -2,13 +2,13 @@ import helpers.MarcoSpecification
 import marco.lang.MarcoNumber
 
 class Lists extends MarcoSpecification {
-    def "first"() {
+    def "first works for lists"() {
         expect:
         eval(/ (first (quote (1 2))) /) == new MarcoNumber(1)
     }
 
-    def "second"() {
+    def "second works for lists"() {
         expect:
-        eval(/ (first (second (quote (1 2 3))) /) == new MarcoNumber(2)
+        eval(/ (first (second (quote (1 2 3)))) /) == new MarcoNumber(2)
     }
 }
