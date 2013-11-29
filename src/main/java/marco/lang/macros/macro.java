b609305ac822553830458bb8e8c51f14a5c46eef
Juan Ibiapina <juanibiapina@gmail.com>
2013-11-29 11:31:00 -0200

    Revert "Add variadic macros."
    This reverts commit 2158f1543671d80a5e848b944fd77b0e536963ca.

--- src/main/java/marco/lang/macros/macro.java
+++ src/main/java/marco/lang/macros/macro.java
@@ -1,8 +1,8 @@
 package marco.lang.macros;
 
+import marco.internal.Cast;
 import marco.internal.Environment;
 import marco.lang.*;
-import marco.lang.exception.MarcoTypeError;
 
 import java.util.List;
 
@@ -11,20 +11,11 @@ public class macro extends MarcoNativeMacro {
     public MarcoObject call(Environment environment, MarcoList arguments) {
         assertArity(2, arguments.size());
 
-        MarcoObject formal = arguments.get(0);
+        MarcoList formal = Cast.toList(arguments.get(0));
+        List<String> formalList = formal.asArgumentList();
 
-        if (formal instanceof MarcoList) {
-            List<String> formalList = ((MarcoList) formal).asArgumentList();
-            MarcoObject body = arguments.get(1);
-            return new MarcoMacro(environment, formalList, body);
-        }
+        MarcoObject body = arguments.get(1);
 
-        if (formal instanceof MarcoSymbol) {
-            String arg = ((MarcoSymbol) formal).getValue();
-            MarcoObject body = arguments.get(1);
-            return new MarcoVariadicMacro(environment, arg, body);
-        }
-
-        throw new MarcoTypeError(MarcoList.class, formal);
+        return new MarcoMacro(environment, formalList, body);
     }
 }
