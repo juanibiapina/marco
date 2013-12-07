e4fdcd191a176a6cec53369f71cc4c34c1385283
Juan Ibiapina <juanibiapina@gmail.com>
2013-12-07 13:36:27 -0200

    Add a runner for files.

--- src/main/java/marco/main.java
+++ src/main/java/marco/main.java
@@ -1,10 +1,23 @@
 package marco;
 
 import marco.repl.Repl;
+import marco.runner.Runner;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        Repl repl = new Repl();
-        repl.start();
+        if (args.length == 0) {
+            Repl repl = new Repl();
+            repl.start();
+            return;
+        }
+
+        if (args.length == 1) {
+            String fileName = args[0];
+            Runner runner = new Runner();
+            runner.run(fileName);
+            return;
+        }
+
+        throw new RuntimeException("Wrong number of arguments");
     }
 }
---
+++ src/main/java/marco/runner/Runner.java
@@ -0,0 +1,26 @@
+package marco.runner;
+
+import marco.internal.Environment;
+import marco.lang.MarcoProgram;
+import marco.lang.exception.MarcoException;
+import marco.parser.Parser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Runner {
+    private Environment environment = Environment.initial();
+
+    public void run(String fileName) {
+        Parser parser = new Parser();
+        MarcoProgram program;
+
+        try {
+            program = parser.parse(new FileInputStream(fileName));
+        } catch (FileNotFoundException e) {
+            throw new MarcoException("File not found: " + fileName);
+        }
+
+        program.eval(environment);
+    }
+}
