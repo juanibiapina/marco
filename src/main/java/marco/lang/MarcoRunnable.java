af9a5696e7556be8a8b6f990e57464416bde64fb
Juan Ibiapina <juanibiapina@gmail.com>
2013-12-09 09:59:47 -0200

    Remove unnused method.

--- src/main/java/marco/lang/MarcoRunnable.java
+++ src/main/java/marco/lang/MarcoRunnable.java
@@ -2,7 +2,6 @@ package marco.lang;
 
 import marco.internal.Environment;
 import marco.lang.contracts.Contract;
-import marco.lang.exception.ContractViolation;
 
 public abstract class MarcoRunnable extends MarcoValue {
     private Contract contract;
@@ -18,10 +17,4 @@ public abstract class MarcoRunnable extends MarcoValue {
     }
 
     protected abstract MarcoObject performInvoke(Environment environment, MarcoList arguments);
-
-    protected void assertArity(int expected, int actual) {
-        if (actual != expected) {
-            throw new ContractViolation(expected, actual);
-        }
-    }
 }
