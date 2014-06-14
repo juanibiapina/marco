cbabdb44ebe52000215e1e69f5aab9e985c6602a
Juan Ibiapina <juanibiapina@gmail.com>
2014-06-14 15:25:53 -0300

    Remove duplicated test.

--- src/test/groovy/marco/core.groovy
+++ src/test/groovy/marco/core.groovy
@@ -2,16 +2,6 @@ package marco
 import helpers.MarcoSpecification
 
 class core extends MarcoSpecification {
-    def "head"() {
-        expect:
-        eval(/ (head [1 2 3 4]) /) == eval(/ 1 /)
-    }
-
-    def "tail"() {
-        expect:
-        eval(/ (tail [1 2 3]) /) == eval(/ [2 3] /)
-    }
-
     def "map"() {
         expect:
         eval(/ (map (function [:x] { (+ x 1) }) [1 2 3]) /) == eval(/ [2 3 4] /)
