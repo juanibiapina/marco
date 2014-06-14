a200e630e44f374bd1ff2ae7afb1fca4f4465b8e
Juan Ibiapina <juanibiapina@gmail.com>
2014-06-14 15:34:38 -0300

    Add a runner test for multiline files.

--- src/test/groovy/marco/runner/RunnerTest.groovy
+++ src/test/groovy/marco/runner/RunnerTest.groovy
@@ -13,4 +13,16 @@ class RunnerTest extends MarcoSpecification {
         then:
         runner.runtime.environment.get("simple").value == eval(/ 3 /)
     }
+
+    def "runs a multi line file"() {
+        given:
+        def runner = new Runner()
+
+        when:
+        runner.run("fixtures/multiline.mrc")
+
+        then:
+        runner.runtime.environment.get("v1").value == eval(/ 3 /)
+        runner.runtime.environment.get("v2").value == eval(/ 4 /)
+    }
 }
